016a45dee6e7aedf9c0de4b57a9f5d7f8f2657c7
Agrim Rawat <rawat.agrim432@gmail.com>
2024-09-17 20:06:37 +0530

    Updated Search Bar - Click on icon focuses - CSS Changes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
-import SearchButton from "../SearchButton/SearchButton";
-import SearchInput from "../SearchInput/SearchInput";
+import { useRef } from "react";
+import ActionButton from "../ActionButton/ActionButton";
 import "./SearchBar.css";
 
 type Props = {
@@ -10,11 +10,39 @@ type Props = {
 const SearchBar = (props: Props) => {
   const { searchTerm, setSearchTerm } = props;
 
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const focusSearchInput = () => {
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  };
+
   return (
     <div className="searchBar">
-      <div className="searchBox">
-        <SearchButton />
-        <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="searchWrapper">
+        <ActionButton onClick={focusSearchInput}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              fill="currentColor"
+              d="M15.009 13.805h-.636l-.22-.219a5.184 5.184 0 0 0 1.256-3.386 5.207 5.207 0 1 0-5.207 5.208 5.183 5.183 0 0 0 3.385-1.255l.221.22v.635l4.004 3.999 1.194-1.195-3.997-4.007zm-4.808 0a3.605 3.605 0 1 1 0-7.21 3.605 3.605 0 0 1 0 7.21z"
+            ></path>
+          </svg>
+        </ActionButton>
+        <input
+          ref={searchInputRef}
+          name="searchInput"
+          className="searchInput"
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
       </div>
     </div>
   );
