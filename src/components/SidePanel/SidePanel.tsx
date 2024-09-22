f467a970d7c51451acf3eddded76815fb6118b1f
Agrim Rawat <rawat.agrim432@gmail.com>
2024-09-22 08:51:45 +0530

    User can start conversation

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,34 +1,71 @@
+import type { User } from "../../types/types";
 import DEFAULT_CLIENT from "../../constant/defaultClient";
 import { useState } from "react";
 import { useUsersContext } from "../../contexts/usersContext";
+import ActionButton from "../ActionButton/ActionButton";
 import Avatar from "../Avatar/Avatar";
 import Conversations from "../Conversations/Conversations";
 import Header from "../Header/Header";
+import NewChatModal from "../NewChatModal/NewChatModal";
 import SearchBar from "../SearchBar/SearchBar";
 import searchUsersByName from "../../utils/searchUsersByName";
 import "./SidePanel.css";
 
 const SidePanel = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const { users } = useUsersContext();
+  const [isChatModalOpen, setIsChatModalOpen] = useState<boolean>(false);
+  const { users, usersDispatch } = useUsersContext();
 
   const otherUsers = users.filter((user) => user.id !== DEFAULT_CLIENT.id);
   const searchUsers = searchUsersByName(otherUsers, searchTerm);
 
+  const createNewChat = async (name: string) => {
+    const image = await fetch(
+      "https://ui-avatars.com/api/?" +
+        new URLSearchParams({
+          name: name,
+          size: "200",
+        }),
+    ).then((res) => res.blob());
+
+    const newUser: User = {
+      id: window.crypto.randomUUID(),
+      name: name,
+      profileImg: URL.createObjectURL(image),
+    };
+
+    usersDispatch({
+      type: "ADD_USER",
+      payload: newUser,
+    });
+    setIsChatModalOpen(false);
+  };
+
   return (
-    <div className="sidePanel">
-      <Header>
-        <Avatar
-          src={DEFAULT_CLIENT.profileImg}
-          alt={DEFAULT_CLIENT.name}
+    <>
+      <div className="sidePanel">
+        <Header>
+          <Avatar
+            src={DEFAULT_CLIENT.profileImg}
+            alt={DEFAULT_CLIENT.name}
+          />
+        </Header>
+        <SearchBar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
         />
-      </Header>
-      <SearchBar
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-      />
-      <Conversations searchUsers={searchUsers} />
-    </div>
+        <Conversations searchUsers={searchUsers} />
+        <ActionButton onClick={() => setIsChatModalOpen(true)}>
+          Start New Chat
+        </ActionButton>
+      </div>
+      {isChatModalOpen && (
+        <NewChatModal
+          closeChatModal={() => setIsChatModalOpen(false)}
+          createChatMethod={createNewChat}
+        ></NewChatModal>
+      )}
+    </>
   );
 };
 
