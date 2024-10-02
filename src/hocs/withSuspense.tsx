1c1d43c4f1f15100530b48d71d0fe9591e52fdd1
Agrim Rawat <rawat.agrim432@gmail.com>
2024-10-03 04:25:24 +0530

    Fix: making with suspense not anonymous

--- src/hocs/withSuspense.tsx
+++ src/hocs/withSuspense.tsx
@@ -2,12 +2,19 @@ import type { ComponentType, ReactNode } from "react";
 
 import { Suspense } from "react";
 
-const withSuspense =
-  <P extends object>(Component: ComponentType<P>, fallback: ReactNode) =>
-  (props: P) => (
-    <Suspense fallback={fallback}>
-      <Component {...props} />
-    </Suspense>
-  );
+function withSuspense<P extends object>(
+  Component: ComponentType<P>,
+  fallback: ReactNode,
+) {
+  const ComponentWithSuspense = (props: P) => {
+    return (
+      <Suspense fallback={fallback}>
+        <Component {...props} />
+      </Suspense>
+    );
+  };
+
+  return ComponentWithSuspense;
+}
 
 export default withSuspense;
